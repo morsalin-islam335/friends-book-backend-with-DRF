4c15df98258968f17d0d233c94d5b2382d1085de
morsalin-islam5350 <weshallovercome335@gmail.com>
2025-01-29 21:04:54 +0600

    create personalDetails model

--- personal_details/models.py
+++ personal_details/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+
+
+class PersonalDetails(models.MOddel):
+    profession = models.TextField(max_length=40, null = True, blank = True)
+    dateOfBirth = models.DateField()
+
+    
