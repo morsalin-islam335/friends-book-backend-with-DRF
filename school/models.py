2ab6a29b3a73691816f33b8064607c45eadd0f88
morsalin-islam5350 <weshallovercome335@gmail.com>
2025-01-29 22:38:46 +0600

    make school models and register it from admin.py

--- school/models.py
+++ school/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+from personal_details.models import PersonalDetails
+
+
+
+class School(models.Model):
+    isGraduate = models.BooleanField(verbose_name = "Is Graduate")
+    startDate = models.DateField(verbose_name = "Start Date")
+    expectedGraduateDate = models.DateField(verbose_name = "Expected Graduate Date")
+    PersonalDetails = models.ForeignKey(PersonalDetails, on_delete= models.CASCADE, null = True, blank = True)
+    
+
