7716e6e38db44c9882c71163f86ebb52cac22ccf
morsalin-islam5350 <weshallovercome335@gmail.com>
2025-01-30 23:41:32 +0600

    make experience model and register it form admin

--- experience/models.py
+++ experience/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+from person.models import Person
+
+class Experience(models.Model):
+    descriptin = models.TextField(max_length=120)
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    
