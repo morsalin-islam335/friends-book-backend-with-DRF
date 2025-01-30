a46c869069627ade70c9e846cb84531ecfc94d46
morsalin-islam5350 <weshallovercome335@gmail.com>
2025-01-31 00:55:25 +0600

    dummy urls add for testing in another apps

--- friends_book_backend_with_DRF/urls.py
+++ friends_book_backend_with_DRF/urls.py
@@ -17,6 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("main.urls")),
@@ -35,3 +41,7 @@ urlpatterns = [
     path("viewer/", include("viewer.urls")),
     path("person/", include("person.urls")),
 ]
+
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
---
+++ person/urls.py
@@ -0,0 +1,10 @@
+
+
+
+from django.urls import path
+
+from . views import dummy
+
+urlpattern =[
+    path("", dummy )
+]
