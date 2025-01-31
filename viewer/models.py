f42ca282f180a6096482af42748cda08f6859c56
morsalin-islam5350 <weshallovercome335@gmail.com>
2025-01-31 23:22:09 +0600

    create story model

--- viewer/models.py
+++ viewer/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+from person.models import Person
+from story.models import Story
+
+class Viewer(models.Model):
+    person = models.ForeignKey(Person, related_name= "viewers", on_delete = models.CASCADE)
+
+    story = models.ForeignKey(Story, on_delete = models.CASCADE)
+
