a212a550608c61c09482cca2237bdeaf53db3ffa
morsalin-islam5350 <weshallovercome335@gmail.com>
2025-01-31 15:46:16 +0600

    create video and tag models and register them from admin

--- video/admin.py
+++ video/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+
+from . models import Video, Tag 
+
+
+admin.site.register(Video)
+admin.site.register(Tag)
--- video/models.py
+++ video/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
+from django.core.exceptions import ValidationError
+import os
+
 # Create your models here.
+
+from person.models import Person
+
+
+
+def validate_video_file(value):
+    """Validate that the uploaded file is a video."""
+    ext = os.path.splitext(value.name)[1].lower()  # Get file extension
+    valid_extensions = ['.mp4', '.avi', '.mov', '.mkv', '.flv', '.wmv']
+    if ext not in valid_extensions:
+        raise ValidationError('Unsupported file format. Please upload a video file.')
+
+
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+    
+
+class Video(models.Model):
+    author = models.ForeignKey(Person, on_delete=models.CASCADE, related_name="videos")
+    title = models.CharField(max_length=255, null = True, blank = True) # in ERD it named as caption
+
+    description = models.TextField(blank=True, null=True)
+    tags = models.ManyToManyField(Tag, blank=True)
+    video = models.FileField(upload_to="uploads/videos/", validators=[validate_video_file])
+    thumbnail = models.ImageField(upload_to="uploads/thumbnails/", blank=True, null=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    views = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.title
