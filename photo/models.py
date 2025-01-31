90aa7228404a65a3f69e1e302efb9695dde1d1df
morsalin-islam5350 <weshallovercome335@gmail.com>
2025-01-31 15:28:46 +0600

    make post model and register it by admin

--- photo/models.py
+++ photo/models.py
@@ -4,7 +4,14 @@ from django.db import models
 
 from person.models import Person
 
+# from post.models import Post
+
+
+
 class Photo(models.Model):
     image = models.ImageField(upload_to="photo/uploads/")
     author = models.ForeignKey(Person, on_delete = models.CASCADE)
+
+    # post = models.ForeignKey(Post, on_delete= models.CASCADE, null = True, blank = True) # a post may  have multiple photo/s
+
     
--- post/models.py
+++ post/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+from person.models import Person
+
+class Post(models.Model):
+    author = models.ForeignKey(Person, on_delete = models.CASCADE)
+    description = models.CharField(max_length=2000, null = True, blank = True)
+    #photo
+    #video
+    #emoji
+    #comment
+    
