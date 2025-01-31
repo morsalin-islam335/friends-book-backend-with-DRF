81f568934b06fa6ed62fdd655b9d8decb9f0b361
morsalin-islam5350 <weshallovercome335@gmail.com>
2025-01-31 15:20:53 +0600

    create photo models and register it

--- photo/models.py
+++ photo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+
+from person.models import Person
+
+class Photo(models.Model):
+    image = models.ImageField(upload_to="photo/uploads/")
+    author = models.ForeignKey(Person, on_delete = models.CASCADE)
+    
