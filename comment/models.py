86259b3c9038ffcc7d6ec93f3555a9451ac4c888
morsalin-islam5350 <weshallovercome335@gmail.com>
2025-01-31 19:38:58 +0600

    complete comment model

--- comment/models.py
+++ comment/models.py
@@ -3,6 +3,21 @@ from django.db import models
 # Create your models here.
 
 
+from person.models import Person
+from photo.models import Photo
+from video.models import Video
+from post.models import Post 
+
 
 class Comment(models.Model):
-    pass
+    
+   person = models.ForeignKey(Person, related_name = "comments", on_delete = models.CASCADE, null = True, blank = True) # after fix TODO resolve this 
+   photo = models.ForeignKey(Photo, on_delete = models.CASCADE, null = True, blank = True, related_name = "comments" )
+   video = models.ForeignKey(Video, on_delete = models.CASCADE, null = True, blank = True, related_name = "comments" )
+   post = models.ForeignKey(Post, on_delete = models.CASCADE, null = True, blank = True, related_name = "comments" )
+
+   parent = models.ForeignKey("self", on_delete = models.CASCADE, null = True, blank = True, related_name = "replaies")
+
+   def __str__(self):
+      return f"{self.person} - {self.text[:30]}"
+   
