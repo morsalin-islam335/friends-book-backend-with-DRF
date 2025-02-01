ac1bd4013e17f618ddbd3972960f331e15e1c771
morsalin-islam5350 <weshallovercome335@gmail.com>
2025-02-01 23:21:22 +0600

    make Emoji serializer

---
+++ likeAndEmoji/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from .models import Emoji
+
+
+class EmojiSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Emoji
+        field = "__all__"
+        
