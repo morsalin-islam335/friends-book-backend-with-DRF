7f7cef1f3d5f03d68b98b212923cfd8015c8cd89
morsalin-islam5350 <weshallovercome335@gmail.com>
2025-02-02 21:22:53 +0600

    add else block in post method

--- api/views.py
+++ api/views.py
@@ -42,3 +42,6 @@ def tags(request):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status = status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+            
