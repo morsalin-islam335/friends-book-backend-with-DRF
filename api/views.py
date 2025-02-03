94c800271c507b24c84c491abd828fd7b8a7a868
morsalin-islam5350 <weshallovercome335@gmail.com>
2025-02-03 21:38:02 +0600

    handle post request in ExperienceListView

--- api/views.py
+++ api/views.py
@@ -186,6 +186,16 @@ class ExperienceListView(APIView):
         serializer = ExperienceSerializer(experience, many = True)
         return Response(serializer.data, status = status.HTTP_200_OK)
 
+    def post(self, request, **kwargs):
+        serializer = ExperienceSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+        else:
+            return Response(status = status.HTTP_400_BAD_REQUEST)
+
+
+        
 
 
 
